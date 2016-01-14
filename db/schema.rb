256243cc92b5f294861136159198e6979e291d66
Ozzie Gooen <Ozzie@grandroundshealth.com>
2016-01-14 15:18:25 -0800

    Added timestamps as migration to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013061949) do
+ActiveRecord::Schema.define(version: 20160114231524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,12 @@ ActiveRecord::Schema.define(version: 20151013061949) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "picture"
-    t.string "auth0_id"
-    t.string "username"
+    t.string   "name"
+    t.string   "picture"
+    t.string   "auth0_id"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
