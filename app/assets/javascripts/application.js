dd595c82011e6a9bab2231cf560906febf508999
Ozzie Gooen <Ozzie@grandroundshealth.com>
2016-02-14 23:09:56 -0800

    Removed dependencies from js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,3 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
