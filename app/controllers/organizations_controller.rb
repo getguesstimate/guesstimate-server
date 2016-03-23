61f8705394f64bad8bd570c1c0d37e64a7a6166b
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-23 11:54:17 -0700

    Gating show on members only for now.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,15 @@
 class OrganizationsController < ApplicationController
   def show
+    unless current_user
+      head :unauthorized
+      return
+    end
+
     @organization = Organization.find(params[:id])
-    render json: OrganizationRepresenter.new(@organization).to_json
+    if @organization.members.exists? current_user
+      render json: OrganizationRepresenter.new(@organization).to_json
+    else
+      head :unauthorized
+    end
   end
 end
