2100cab75c3d2f5b9909797ea9079d5a98d1ac57
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-24 13:54:03 -0700

    Adding question.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,15 +1,13 @@
 class OrganizationsController < ApplicationController
+  # TODO(matthew): Are there helpers for logged_in checking? Can we run a before_action here and elsewhere to strip the
+  # current_user checks?
   def show
-    unless current_user
+    @organization = Organization.find(params[:id])
+    unless current_user && current_user.member_of?(@organization.id)
       head :unauthorized
       return
     end
 
-    @organization = Organization.find(params[:id])
-    if current_user.member_of? @organization.id
-      render json: OrganizationRepresenter.new(@organization).to_json
-    else
-      head :unauthorized
-    end
+    render json: OrganizationRepresenter.new(@organization).to_json
   end
 end
