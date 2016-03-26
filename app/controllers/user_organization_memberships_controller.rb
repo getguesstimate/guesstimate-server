63785d7c7a6766b39f71e2c346088428cf6f2c0a
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-25 19:48:21 -0700

    Opening up memberships querying to public.

--- app/controllers/user_organization_memberships_controller.rb
+++ app/controllers/user_organization_memberships_controller.rb
@@ -1,6 +1,4 @@
 class UserOrganizationMembershipsController < ApplicationController
-  before_action :authenticate
-
   def user_memberships
     # We use a UserOrganizationMemberships scope here to avoid unnecessary DB indirection through the user.
     @memberships = UserOrganizationMembership.for_user(params[:user_id])
@@ -8,11 +6,6 @@ class UserOrganizationMembershipsController < ApplicationController
   end
 
   def organization_memberships
-    unless current_user.member_of?(params[:organization_id])
-      head :unauthorized
-      return
-    end
-
     # We use a UserOrganizationMemberships scope here to avoid unnecessary DB indirection through the organization.
     @memberships = UserOrganizationMembership.for_organization(params[:organization_id])
     render json: OrganizationMembershipsRepresenter.new(@memberships).to_json
