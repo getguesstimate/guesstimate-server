864ec82742e632b8d4858e18002e2f0266f612f0
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-25 20:13:39 -0700

    Adding params to the controller and updating the migration file to also store email_verified.

--- db/migrate/20160326031110_add_email_email_verified_gender_locale_location_to_users.rb
+++ db/migrate/20160326031110_add_email_email_verified_gender_locale_location_to_users.rb
@@ -1,8 +1,9 @@
-class AddEmailGenderLocaleLocationToUsers < ActiveRecord::Migration
+class AddEmailEmailVerifiedGenderLocaleLocationToUsers < ActiveRecord::Migration
   def change
     add_column :users, :email, :string
     add_column :users, :gender, :string
     add_column :users, :locale, :string
     add_column :users, :location, :string
+    add_column :users, :email_verified, :bool
   end
 end
