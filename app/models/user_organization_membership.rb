bf058a5a96e428dbcc11a12fb278f95bf7f26336
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-01 10:43:51 -0700

    Adds uniqueness constraint on memberships and tests thereof.

--- app/models/user_organization_membership.rb
+++ app/models/user_organization_membership.rb
@@ -3,6 +3,7 @@ class UserOrganizationMembership < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :organization, :user
+  validates_uniqueness_of :organization_id, scope: :user_id
 
   scope :for_organization, -> (organization_id) { where(organization_id: organization_id) }
   scope :for_user, -> (user_id) { where(user_id: user_id) }
--- spec/models/user_organization_membership_spec.rb
+++ spec/models/user_organization_membership_spec.rb
@@ -17,5 +17,18 @@ RSpec.describe UserOrganizationMembership, type: :model do
       let (:organization) { nil }
       it { is_expected.to_not be_valid }
     end
+
+    context 'multiple memberships' do
+      let (:user2) { FactoryGirl.create :user }
+      it 'should be valid with no collision' do
+        FactoryGirl.create :user_organization_membership, user: user2, organization: organization
+        is_expected.to be_valid
+      end
+
+      it 'should not be valid with collision' do
+        FactoryGirl.create :user_organization_membership, user: user, organization: organization
+        is_expected.to_not be_valid
+      end
+    end
   end
 end
