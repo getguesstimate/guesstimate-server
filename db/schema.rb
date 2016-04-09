5a581b4a80d63863fc3d3bc58455f7939637873f
Ozzie Gooen <ozzieagooen@gmail.com>
2016-04-09 01:04:42 -0700

    When a model is deleted user should go to owners page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406022345) do
+ActiveRecord::Schema.define(version: 20160409041418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160406022345) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   create_table "spaces", force: :cascade do |t|
