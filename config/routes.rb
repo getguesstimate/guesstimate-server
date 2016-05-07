0b90b2d64afdf0525517cd0f0ba4e2dc50c4b468
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-07 14:21:28 -0700

    Locally working caching.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   mount Knock::Engine => '/knock'
 
-  resources :spaces, only: [:show, :create, :update, :destroy]
+  resources :spaces, only: [:show, :create, :update, :destroy], format: :json
   resources :spaces do
-    resources :copies, only: [:create]
+    resources :copies, only: [:create], format: :json
   end
 
   resources :users do
-    resources :spaces, only: [:index]
-    get :memberships, to: 'user_organization_memberships#user_memberships'
-    post '/account/synchronization', to: 'accounts#synchronization'
-    get '/account/new_subscription_iframe', to: 'accounts#new_subscription_iframe'
+    resources :spaces, only: [:index], format: :json
+    get :memberships, to: 'user_organization_memberships#user_memberships', format: :json
+    post '/account/synchronization', to: 'accounts#synchronization', format: :json
+    get '/account/new_subscription_iframe', to: 'accounts#new_subscription_iframe', format: :json
   end
 
-  resources :organization, only: [:show]
+  resources :organization, only: [:show], format: :json
   resources :organizations do
-    resources :spaces, only: [:index]
-    get :members, to: 'user_organization_memberships#organization_memberships'
+    resources :spaces, only: [:index], format: :json
+    get :members, to: 'user_organization_memberships#organization_memberships', format: :json
   end
 end
