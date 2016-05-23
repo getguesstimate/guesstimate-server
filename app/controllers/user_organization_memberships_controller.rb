51758dd7aae49e46190aea1a5f848f981d247b1c
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-23 16:50:02 -0700

    Basic setup for membership deletion.

--- app/controllers/user_organization_memberships_controller.rb
+++ app/controllers/user_organization_memberships_controller.rb
@@ -1,4 +1,5 @@
 class UserOrganizationMembershipsController < ApplicationController
+  before_action :authenticate, only: [:destroy]
   def user_memberships
     # We use a UserOrganizationMemberships scope here to avoid unnecessary DB indirection through the user.
     @memberships = UserOrganizationMembership.for_user(params[:user_id])
@@ -9,5 +10,12 @@ class UserOrganizationMembershipsController < ApplicationController
     # We use a UserOrganizationMemberships scope here to avoid unnecessary DB indirection through the organization.
     @memberships = UserOrganizationMembership.for_organization(params[:organization_id])
     render json: OrganizationMembershipsRepresenter.new(@memberships).to_json
+  end
+
+  def destroy
+    @membership = UserOrganizationMembership.find(params[:id])
+    head :unauthorized unless current_user.id == @membership.organization.admin_id
+    @membership.destroy
+    head :no_content
   end
 end
