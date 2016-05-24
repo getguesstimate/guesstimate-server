641ed9f50d52136b2d535882037fd4c1482cf714
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-24 14:28:00 -0700

    Refactoring memberships controller spec.

--- spec/controllers/user_organization_memberships_spec.rb
+++ spec/controllers/user_organization_memberships_spec.rb
@@ -12,60 +12,51 @@ end
 RSpec.describe UserOrganizationMembershipsController, type: :controller do
   describe 'DELETE destroy' do
     let (:membership) { FactoryGirl.create(:user_organization_membership) }
+    let (:requesting_user) { nil }
+    before do
+      requesting_user && setup_knock(requesting_user)
+      delete :destroy, id: membership.id
+    end
 
-    context 'when logged out' do
-      before { delete :destroy, id: membership.id }
-
+    shared_examples 'delete failed' do
       it { is_expected.to respond_with :unauthorized }
       it 'should not have deleted the membership' do
         expect(UserOrganizationMembership.find(membership.id)).to eq membership
       end
     end
 
-    context 'when logged in' do
-      let (:requesting_user) { nil } # overwritten in each context below
-      before do
-        setup_knock(requesting_user)
-        delete :destroy, id: membership.id
+    context 'for logged out requester' do
+      include_examples 'delete failed'
+    end
+
+    context 'for admin requester' do
+      let (:requesting_user) {
+        user = FactoryGirl.create(:user)
+        organization = membership.organization
+        FactoryGirl.create(:user_organization_membership, user: user, organization: organization)
+        organization.update(admin: user)
+        user
+      }
+
+      it { is_expected.to respond_with :no_content }
+      it 'should have deleted the membership' do
+        expect { UserOrganizationMembership.find(membership.id) }.to raise_exception(ActiveRecord::RecordNotFound)
       end
+    end
 
-      context 'when admin' do
-        let (:requesting_user) {
-          user = FactoryGirl.create(:user)
-          organization = membership.organization
-          FactoryGirl.create(:user_organization_membership, user: user, organization: organization)
-          organization.update(admin: user)
-          user
-        }
+    context 'for member (but not admin)' do
+      let (:requesting_user) {
+        user = FactoryGirl.create(:user)
+        organization = membership.organization
+        FactoryGirl.create(:user_organization_membership, user: user, organization: organization)
+        user
+      }
+      include_examples 'delete failed'
+    end
 
-        it { is_expected.to respond_with :no_content }
-        it 'should have deleted the membership' do
-          expect { UserOrganizationMembership.find(membership.id) }.to raise_exception(ActiveRecord::RecordNotFound)
-        end
-      end
-
-      context 'when member but not admin' do
-        let (:requesting_user) {
-          user = FactoryGirl.create(:user)
-          organization = membership.organization
-          FactoryGirl.create(:user_organization_membership, user: user, organization: organization)
-          user
-        }
-
-        it { is_expected.to respond_with :unauthorized }
-        it 'should not have deleted the membership' do
-          expect(UserOrganizationMembership.find(membership.id)).to eq membership
-        end
-      end
-
-      context 'when not member' do
-        let (:requesting_user) { FactoryGirl.create(:user) }
-
-        it { is_expected.to respond_with :unauthorized }
-        it 'should not have deleted the membership' do
-          expect(UserOrganizationMembership.find(membership.id)).to eq membership
-        end
-      end
+    context 'when not member' do
+      let (:requesting_user) { FactoryGirl.create(:user) }
+      include_examples 'delete failed'
     end
   end
 end
