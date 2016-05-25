635e42516972471e94b3b094c6e50f309fba1428
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-25 14:21:20 -0700

    Added a basic add member route for organizations via email. Only works for existent users right now (returns 404 otherwise).

--- app/controllers/user_organization_memberships_controller.rb
+++ app/controllers/user_organization_memberships_controller.rb
@@ -1,7 +1,8 @@
 class UserOrganizationMembershipsController < ApplicationController
-  before_action :authenticate, only: [:destroy]
+  before_action :authenticate, only: [:create_by_email, :destroy]
   before_action :set_membership, only: [:destroy]
-  before_action :check_authorization, only: [:destroy]
+  before_action :set_organization, only: [:create_by_email]
+  before_action :check_authorization, only: [:create_by_email, :destroy]
 
   def user_memberships
     # We use a UserOrganizationMemberships scope here to avoid unnecessary DB indirection through the user.
@@ -15,6 +16,21 @@ class UserOrganizationMembershipsController < ApplicationController
     render json: OrganizationMembershipsRepresenter.new(@memberships).to_json
   end
 
+  def create_by_email
+    user = User.find_by_email params[:email]
+    if user.nil?
+      head 404
+      return
+    end
+
+    @membership = UserOrganizationMembership.new user: user, organization_id: params[:organization_id]
+    if @membership.save
+      render json: OrganizationMembershipRepresenter.new(@membership).to_json
+    else
+      render json: @membership.errors, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @membership.destroy
     head :no_content
@@ -24,9 +40,14 @@ class UserOrganizationMembershipsController < ApplicationController
 
   def set_membership
     @membership = UserOrganizationMembership.find(params[:id])
+    @organization = @membership.organization
+  end
+
+  def set_organization
+    @organization = Organization.find(params[:organization_id])
   end
 
   def check_authorization
-    head :unauthorized unless current_user.id == @membership.organization.admin_id
+    head :unauthorized unless current_user.id == @organization.admin_id
   end
 end
