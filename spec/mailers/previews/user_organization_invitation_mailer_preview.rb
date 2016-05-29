f6cc73983c77cf0cb1b73e421af367276af4cc44
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-29 16:37:21 -0700

    Fixing a missed merge conflict.

--- spec/mailers/previews/user_organization_invitation_mailer_preview.rb
+++ spec/mailers/previews/user_organization_invitation_mailer_preview.rb
@@ -1,15 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_organization_membership_mailer
-<<<<<<< HEAD:spec/mailers/previews/user_organization_invitation_mailer_preview.rb
 class UserOrganizationInvitationMailerPreview < ActionMailer::Preview
-  # TODO(matthew): Configure later.
-=======
-class UserOrganizationMembershipMailerPreview < ActionMailer::Preview
   def new_user_invite
-    organization = FactoryGirl.create(:organization)
-    user = FactoryGirl.create(:user)
-    password = "password"
-    redirect_url = "fake"
-    UserOrganizationMembershipMailer.new_user_invite user, organization, redirect_url, password
+    invitation = FactoryGirl.create(:user_organization_invitation)
+    UserOrganizationMembershipMailer.new_user_invite invitation
   end
->>>>>>> master:spec/mailers/previews/user_organization_membership_mailer_preview.rb
 end
