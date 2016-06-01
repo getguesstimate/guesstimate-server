1258541efc5161361e074b8d706d705feca55a89
Matthew McDermott <mattmcdermott8@gmail.com>
2016-06-01 12:34:54 -0700

    Name changed, profile fixed. Need to test with real email.

--- spec/mailers/previews/user_organization_invitation_mailer_preview.rb
+++ spec/mailers/previews/user_organization_invitation_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:4000/rails/mailers/user_organization_invitation_mailer
 class UserOrganizationInvitationMailerPreview < ActionMailer::Preview
   def new_user_invite
-    invitation = FactoryGirl.create(:user_organization_invitation)
+    invitation = UserOrganizationInvitation.first
     UserOrganizationInvitationMailer.new_user_invite invitation
   end
 end
