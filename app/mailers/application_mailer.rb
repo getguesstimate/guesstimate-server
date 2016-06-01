2b07e506ec9b88e97bf7b24283d79f4960a41555
Matthew McDermott <mattmcdermott8@gmail.com>
2016-06-01 12:09:40 -0700

    WIP

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@getguesstimate.com"
+  default from: "Guesstimate no-reply@getguesstimate.com"
   layout 'mailer'
 end
--- spec/mailers/previews/user_organization_invitation_mailer_preview.rb
+++ spec/mailers/previews/user_organization_invitation_mailer_preview.rb
@@ -1,4 +1,4 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_organization_membership_mailer
+# Preview all emails at http://localhost:4000/rails/mailers/user_organization_invitation_mailer
 class UserOrganizationInvitationMailerPreview < ActionMailer::Preview
   def new_user_invite
     invitation = FactoryGirl.create(:user_organization_invitation)
