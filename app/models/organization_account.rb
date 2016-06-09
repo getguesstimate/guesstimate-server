8deb369799ad9aad96abe9f9d54b75c8ab94b413
Ozzie Gooen <ozzieagooen@gmail.com>
2016-06-08 23:52:49 -0700

    Removed unecessary comment

--- app/models/organization_account.rb
+++ app/models/organization_account.rb
@@ -5,12 +5,4 @@ class OrganizationAccount < ActiveRecord::Base
     return false unless chargebee_id
     return ExternalSubscriptions::PaymentPortal.new(chargebee_id)
   end
-
-  #Outline for later
-  #def new_trial_subscription()
-    #chargebee_id = "organization-#{organization.id}"
-    #ExternalSubscriptions::NewTrialSubscription.new(chargebee_id, 'organization_basic')
-    #self.update_attribute(:has_payment_account, true)
-    #self.update_attribute(:chargebee_id, chargebee_id)
-  #end
 end
