0f51382d5256fa6a4b4f70d5f3ff4f4e882bec1d
Matthew McDermott <mattmcdermott8@gmail.com>
2016-06-22 14:55:45 -0700

    Added server set up for calculator creation.

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,7 +1,31 @@
 class CalculatorsController < ApplicationController
+  before_action :authenticate, :set_space, :check_authorization, only: [:create]
+
   # GET /calculators/:id
   def show
     @calculator = Calculator.find(params[:id])
     render json: CalculatorRepresenter.new(@calculator).to_json
+  end
+
+  def create
+    @calculator = @space.calculators.new calculator_params
+    if (@calculator.save)
+      render json: CalculatorRepresenter.new(@calculator).to_json
+    else
+      render json: @calculator.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_space
+    @space = Space.find(params[:space_id])
+  end
+
+  def check_authorization
+    head :unauthorized unless @space.editable_by_user? current_user
+  end
+
+  def calculator_params
+    params.require(:calculator).permit(:title, :content, input_ids: [], output_ids: [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :spaces, only: [:show, :create, :update, :destroy]
   resources :spaces do
+    resources :calculators, only: [:create]
     resources :copies, only: [:create]
   end
 
