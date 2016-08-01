4add2b49d376913b18fb4600b510230be3c2acf7
Matthew McDermott <mattmcdermott8@gmail.com>
2016-08-01 15:35:11 -0700

    Adding more validations to fact model.

--- spec/factories/facts.rb
+++ spec/factories/facts.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name 'name'
     sequence(:variable_name) { |n| "variable_#{n}" }
     expression '100'
+    simulation { {"sample" => { "values" => [1], "errors" => [] }, "stats" => { "mean" => 1, "stdev" => 0, "length" => 1 }} }
 
     organization
   end
