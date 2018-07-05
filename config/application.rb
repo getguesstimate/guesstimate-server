29c47d60f50ac275280eeef10bf91bea567bb81d
Ozzie Gooen <Ozzie@grandroundshealth.com>
2018-07-04 22:17:27 -0700

    Adjusted config to test heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module GuesstimateApi
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('lib')
+    config.enable_dependency_loading  = true
   end
 end
