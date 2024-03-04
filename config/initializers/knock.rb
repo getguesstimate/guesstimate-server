42fae801b5482c22f50b383d3074c1e89ba64974
Vyacheslav Matyukhin <me@berekuk.ru>
2024-03-03 21:39:43 -0600

    take audience from config, fix code

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -5,5 +5,5 @@ Knock.setup do |config|
   jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
   config.token_public_key = OpenSSL::X509::Certificate.new(Base64.decode64(jwks_keys[0]['x5c'].first)).public_key
 
-  config.token_audience = -> "guesstimate-api"
+  config.token_audience = -> { Rails.application.secrets.auth0_audience }
 end
